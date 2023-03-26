3b3dad54e3efaae907dfe5aba50cd22145c97a85
Leon <vilovic.leon@gmail.com>
2023-03-26 14:11:09 +0200

    First playable version
    First playable version

--- Assets/LogicManagerScript.cs
+++ Assets/LogicManagerScript.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManagerScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
-    [ContextMenu ("Increase Score")]
+    [ContextMenu("Increase Score")]
     public void addScore()
     {
         playerScore = playerScore + 1;
@@ -16,4 +18,16 @@ public class LogicManagerScript : MonoBehaviour
       
 
     }
+
+    public void restartGame()
+    {
+        Debug.Log("restartGame() function triggerd");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //todo
+    }
+    public void gameOver()
+    {
+            gameOverScreen.SetActive(true);    
+    }
+
 }
