404443a1983bab8f72659ff96efa436fecab2c64
Leon <vilovic.leon@gmail.com>
2025-02-07 20:18:39 +0100

    07022025
    pipes get more narrow as the time goes on

--- Assets/DifficultySettings.cs
+++ Assets/DifficultySettings.cs
@@ -19,12 +19,15 @@ public class DifficultySettings
 
     private float scoreDifficultyMultiplier = 1;
     private int currentScoreThreshold = 400;
+    private float pipeSpawnerPipeOffset = 0f;
     private int[] scoreThresholdArr = new int[] { 900, 1500, 2200, 3000 };
     private int scoreThresholdIndex = 0;
     private PipeSpawnerScrip PipeSpawner = null;
 
     public int CurrentScoreThreshold { get => currentScoreThreshold; set => currentScoreThreshold = value; }
     public float ScoreDifficultyMultiplier { get => scoreDifficultyMultiplier; set => scoreDifficultyMultiplier = value; }
+
+    public float PipeSpawnerPipeOffset { get => pipeSpawnerPipeOffset; set => pipeSpawnerPipeOffset = value; }
 
     public void increasePipeSpawnSpeed()
     {
@@ -36,6 +39,12 @@ public class DifficultySettings
         if (PipeSpawner.spawnRate > 1.5) { PipeSpawner.spawnRate = PipeSpawner.spawnRate * 0.965f; }
 
         Debug.Log("PipeSpawner.SpawnRate updated: " + PipeSpawner.spawnRate);
+
+    }
+
+    public void increasePipeSpawnerPipeOffset()
+    {
+        if (pipeSpawnerPipeOffset < 5.0f) { pipeSpawnerPipeOffset += 0.2f; }
     }
 
     public void increaseDifficulty()
@@ -52,5 +61,7 @@ public class DifficultySettings
         }
         increasePipeSpawnSpeed();
         Debug.Log("increaseDifficulty() currentScoreThreshold:" + currentScoreThreshold.ToString());
+        increasePipeSpawnerPipeOffset();
+        Debug.Log("increasePipeSpawnerPipeOffset():" + pipeSpawnerPipeOffset.ToString());
     }
 }
--- Assets/PipeSpawnerScrip.cs
+++ Assets/PipeSpawnerScrip.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PipeSpawnerScrip : MonoBehaviour
 {
     public GameObject gameObjectForSpawn;
+    public LogicManagerScript logicManagerScript;
 
     public float spawnRate;
 
@@ -20,7 +22,9 @@ public class PipeSpawnerScrip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        logicManagerScript = GameObject.FindGameObjectWithTag("LogicManager").GetComponent<LogicManagerScript>();
         spawnPipe();
+
     }
 
     // Update is called once per frame
@@ -44,6 +48,26 @@ public class PipeSpawnerScrip : MonoBehaviour
         float lowestPoint = transform.position.y - SpawnOffset;
         float highestPoint = transform.position.y + SpawnOffset;
 
-         Instantiate(gameObjectForSpawn, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+
+        Transform child1 = gameObjectForSpawn.transform.GetChild(0);
+        Transform child2 = gameObjectForSpawn.transform.GetChild(1);
+        Transform child3 = gameObjectForSpawn.transform.GetChild(2);
+
+        UnityEngine.Debug.Log("DifficultySettings.Instance.PipeSpawnerPipeOffset: " + DifficultySettings.Instance.PipeSpawnerPipeOffset);
+
+        child1.localPosition += new Vector3(0, -DifficultySettings.Instance.PipeSpawnerPipeOffset, 0);
+        child2.localPosition += new Vector3(0, DifficultySettings.Instance.PipeSpawnerPipeOffset, 0);
+
+        UnityEngine.Debug.Log("child1.localPosition " + child1.localPosition);
+        UnityEngine.Debug.Log("child2.localPosition " + child2.localPosition);
+
+        Instantiate(gameObjectForSpawn, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+
+        //if you dont return gameObjectForSpawn to its initial state the prefab gets alterd in next execution
+        child1.localPosition += new Vector3(0, DifficultySettings.Instance.PipeSpawnerPipeOffset, 0);
+        child2.localPosition += new Vector3(0, -DifficultySettings.Instance.PipeSpawnerPipeOffset, 0);
+ 
+
+
     }
 }
