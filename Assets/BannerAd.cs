fcf4dc9b8915bd6d8252fafbb8fce7b75a6b4b15
Leon <vilovic.leon@gmail.com>
2025-08-17 15:42:53 +0200

    17082025 4

--- Assets/BannerAd.cs
+++ Assets/BannerAd.cs
@@ -8,7 +8,23 @@ public class BannerAd : MonoBehaviour
 
     void Start()
     {
-        MobileAds.Initialize(initStatus => { });
+        // MobileAds.Initialize(initStatus => { });
+
+        MobileAds.Initialize((InitializationStatus initstatus) =>
+        {
+            if (initstatus == null)
+            {
+                Debug.LogError("Google Mobile Ads initialization failed.");
+                return;
+            }
+
+            Debug.Log("Google Mobile Ads initialization complete.");
+
+            // Google Mobile Ads events are raised off the Unity Main thread. If you need to
+            // access UnityEngine objects after initialization,
+            // use MobileAdsEventExecutor.ExecuteInUpdate(). For more information, see:
+            // https://developers.google.com/admob/unity/global-settings#raise_ad_events_on_the_unity_main_thread
+        });
 
         string adUnitId = "ca-app-pub-3940256099942544/6300978111"; // Test ID
 
